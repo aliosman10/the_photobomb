e342f670552d5ba65124b5c10fc27bd7239bfa93
Ali Osman <osmanali966@gmail.com>
2015-08-27 10:46:42 -0400

    first commit

---
+++ photobomb.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'unirest'
+
+get '/' do
+	@title = 'Photos'
+	@photos = recent_photos
+	erb :all_photos
+end
+
+def recent_photos(hashtag = "chelsea", resolution = "thumbnail")
+		@photos = []
+		instagram_response = "https://api.instagram.com/v1/tags/#{hashtag}/media/recent?client_id=f71df6aade944c44a10b2adc9de60f07"
+		response = Unirest.get(instagram_response)
+		photos_array = response.body["data"]
+
+		photos_array.each do |photo|
+			image = photo["images"][resolution]["url"]
+			@photos << image
+		end
+
+		@photos
+end
+
+get '/:resolution/:hashtag' do
+	@title = 'Photos'
+	@photos = recent_photos(params[:hashtag],params[:resolution])
+	erb :all_photos
+end
+
+
+
+
+
+
